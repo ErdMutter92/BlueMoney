14ce0524307c6e84af2971d0bbf4071fabb2199c
ErdMutter92 <alexis@bleauweb.net>
2014-06-27 13:54:45 -0400

    System Recognition /w first run functionality
    fixed firstrun to work, added platform detection for future smart cross
    platform database storage

--- BlueMoney.py
+++ BlueMoney.py
@@ -1,5 +1,14 @@
-import control, view, firstrun, platform
+import control, view, firstrun, platform, os
 
 class BlueMoney:
 
+    system = platform.system()
+    
+    if system == 'darwin':
+        dataDir = '.'
+    else:
+        dataDir = '.'
+
+    if not os.path.exists(dataDir+'/data/firstrun'):
+        setup = firstrun.Firstrun(dataDir)
     view = view.View()
