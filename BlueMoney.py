f089bdf5e547f1f1880bb20fab751cb34f17d67f
ErdMutter92 <alexis@bleauweb.net>
2014-06-27 19:22:51 -0400

    initial work for error handling

--- BlueMoney.py
+++ BlueMoney.py
@@ -1,14 +1,22 @@
-import control, view, firstrun, platform, os
+import control, view, firstrun, platform, os, sys, error, traceback
 
 class BlueMoney:
 
-    system = platform.system()
-    
-    if system == 'darwin':
-        dataDir = '.'
-    else:
-        dataDir = '.'
+    try:
+        system = platform.system()
+        if system == 'darwin':
+            dataDir = '.'
+        else:
+            dataDir = '.'
 
-    if not os.path.exists(dataDir+'/data/firstrun'):
-        setup = firstrun.Firstrun(dataDir)
-    view = view.View()
+        if not os.path.exists(dataDir+'/data/firstrun'):
+            setup = firstrun.Firstrun(dataDir)
+        view = view.View()
+    except IndexError as e:
+        error.report('IndexError', e)
+    except Exception as e:
+        error.report('Unknown Exception', e)
+    except GeneratorExit as e:
+        error.report('System Exit', e)
+    except:
+        error.report('Unknown Error', ':(')
