126d45d1a653386211f1fb6e4172e19568875a77
Alexis Bleau <alexis@bleauweb.net>
2014-06-27 00:10:52 -0400

    Deleted new_module.py
     ... file not needed

--- new_module.py
+++
@@ -1,9 +0,0 @@
-# To change this license header, choose License Headers in Project Properties.
-# To change this template file, choose Tools | Templates
-# and open the template in the editor.
-
-__author__="erdmutter92"
-__date__ ="$Jun 25, 2014 10:09:36 AM$"
-
-if __name__ == "__main__":
-    print "Hello World"
